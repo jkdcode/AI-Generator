8347342e7430b94a5b5904e3fc14213400a8790c
jkdcode <jk.didriksen@gmail.com>
2024-01-26 15:16:12 +0100

    added api functionality + worked on prompt engineering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#generated-poem");
-
+function displayPoem(response) {
+  console.log("Poem generated");
   new Typewriter("#generated-poem", {
-    strings:
-      "My life, - <br> How much more of it remains? <br> The night is brief. <br>",
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
     cursor: "",
   });
+  response.data.answer;
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "7a1b7bb3a54afd03ao8ta3f7b40acb43";
+  let prompt = `User instructions: Please generate a haiku inspired by ${instructionsInput.value}, in basic html. Add "<i>~ SheCodes AI</i>" after the last line of the poem.`;
+  let context =
+    "you know haiku style requirements and tradition. Make sure to follow the user instructions, and to separate each line with a <br> to respect haiku style requirements";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem...");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
