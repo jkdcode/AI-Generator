2c136f2f5a3a7d3dcd58ce0b96103635f3660a3f
jkdcode <jk.didriksen@gmail.com>
2024-01-26 16:53:05 +0100

    added generating-animation, removed console logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Poem generated");
   new Typewriter("#generated-poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -17,12 +16,11 @@ function generatePoem(event) {
   let apiKey = "7a1b7bb3a54afd03ao8ta3f7b40acb43";
   let prompt = `User instructions: Please generate a haiku inspired by ${instructionsInput.value}, in basic html. Add "<i>~ SheCodes AI</i>" after the last line of the poem.`;
   let context =
-    "you know haiku style requirements and tradition. Make sure to follow the user instructions, and to separate each line with a <br> to respect haiku style requirements";
+    "you know haiku style requirements and tradition. Follow the user instructions, and separate each line with a <br> to respect haiku style requirements";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem...");
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
+  let poemElement = document.querySelector("#generated-poem");
+  poemElement.innerHTML = `<div class="generating">Generating...</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
